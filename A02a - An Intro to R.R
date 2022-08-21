0029a204846962e62bea29dee2d907b2045fae7f
Ryne VanKrevelen <rvankrevelen@elon.edu>
2022-08-21 09:54:08 -0400

    Added code for Activity 02a

---
+++ A02a - An Intro to R.R
@@ -0,0 +1,14 @@
+seq(from = 1, to = 100, by = 10)
+?seq
+seq(from = __, to = __, by = __)
+myseq <- ____
+myseq
+myseq / 2
+myseq2 <- myseq / 2
+c(apple, banana, carrot)
+c("apple", "banana", "carrot")
+
+summary(CO2)
+CO2$conc
+CO2$___
+# This is an R comment!
